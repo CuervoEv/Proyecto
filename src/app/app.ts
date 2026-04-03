90430328ed0e5a8c48b7c212c871288af61fe09e
CuervoEv <juliandavidcuervomartinez@gmail.com>
2026-04-03 11:43:45 -0500

    Mover header y footer a carpeta componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
-import { Footer } from './footer/footer';
+import { Header } from './componentes/header/header';
+import { Footer } from './componentes/footer/footer';
 
 @Component({
   selector: 'app-root',
