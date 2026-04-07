b25c1386fa9489507734a7f05ff8f085b100b3de
CuervoEv <juliandavidcuervomartinez@gmail.com>
2026-04-07 13:39:46 -0500

    simplificacion

--- src/app/inventario/components/consumir-stock-modal/consumir-stock-modal.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-consumir-stock-modal',
-  imports: [],
-  templateUrl: './consumir-stock-modal.html',
-  styleUrl: './consumir-stock-modal.css',
-})
-export class ConsumirStockModal {}
